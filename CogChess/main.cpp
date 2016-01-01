f6fab131a61d6cea71345d93b174fa93718289fa
Eric Nichols <epnichols@gmail.com>
2015-12-31 18:22:28 -0800

    Initial Commit

---
+++ CogChess/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CogChess
+//
+//  Created by Eric Nichols on 12/31/15.
+//  Copyright © 2015 Eric Nichols. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
